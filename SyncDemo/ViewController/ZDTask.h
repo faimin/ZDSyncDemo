459683846b1c8599f5bd53056329112841f2d634
Sephiroth <fuxianchao@gmail.com>
2016-05-08 21:34:14 +0800

    zdtask test

---
+++ SyncDemo/ViewController/ZDTask.h
@@ -0,0 +1,13 @@
+//
+//  ZDTask.h
+//  ZDSyncDemo
+//
+//  Created by 符现超 on 16/5/6.
+//  Copyright © 2016年 ZD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZDTask : NSObject
+
+@end
---
+++ SyncDemo/ViewController/ZDTask.m
@@ -0,0 +1,42 @@
+//
+//  ZDTask.m
+//  ZDSyncDemo
+//
+//  Created by 符现超 on 16/5/6.
+//  Copyright © 2016年 ZD. All rights reserved.
+//
+
+#import "ZDTask.h"
+
+typedef void(^BlockAction)();
+typedef void(^GroupResponseFailure)(NSArray * errorArray);
+
+static char groupErrorKey;
+static char queueGroupKey;
+
+const void *key = &key;
+
+@interface ZDTask ()
+
+@end
+
+@implementation ZDTask
+
++ (instancetype)task
+{
+    return [[self alloc] init];
+}
+
+- (void)testWithBlock:(void(^)())completeBlock
+{
+    dispatch_group_t zdGroup = dispatch_group_create();
+    dispatch_queue_t zdQueue = dispatch_queue_create("ZDTaskQueue", DISPATCH_QUEUE_CONCURRENT);
+    
+    
+    dispatch_group_notify(zdGroup, zdQueue, ^{
+        NSLog(@"任务结束");
+        completeBlock();
+    });
+}
+
+@end
